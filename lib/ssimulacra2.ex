c560d0ae1431980368ac410bdcb607896d7d2f35
Håvard Lindset <havard@givn.no>
2026-06-19 22:29:59 +0200

    chore: scaffold mix project and pin toolchain via mise

---
+++ lib/ssimulacra2.ex
@@ -0,0 +1,18 @@
+defmodule Ssimulacra2 do
+  @moduledoc """
+  Documentation for `Ssimulacra2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ssimulacra2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ssimulacra2_test.exs
@@ -0,0 +1,8 @@
+defmodule Ssimulacra2Test do
+  use ExUnit.Case
+  doctest Ssimulacra2
+
+  test "greets the world" do
+    assert Ssimulacra2.hello() == :world
+  end
+end
